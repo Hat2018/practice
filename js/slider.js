3a7bde2c7d24d173f9dd2e839962250279595421
Hat2018 <45607852+Hat2018@users.noreply.github.com>
2019-06-07 07:13:35 +0300

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function() { 
+						   
+	var slides = $(".slider_one .slides").children(".slide_one"); 
+	var width = $(".slider_one .slides").width(); 
+	var i = slides.length; 
+	var offset = i * width;
+	i--; 
+	$(".slider_one .slides").css('width',offset); 
+	
+	offset = 0; 
+	$("body .slider_one .next").click(function(){	
+		if (offset < width * i) {	
+			offset += width; 
+			$(".slider_one .slides").css("transform","translate3d(-"+offset+"px, 0px, 0px)"); 
+		}
+		
+	});
+
+
+	$("body .slider_one .prev").click(function(){	
+		if (offset > 0) { 
+			offset -= width; 
+			$(".slider_one .slides").css("transform","translate3d(-"+offset+"px, 0px, 0px)"); 
+		}
+	});
+
+});
